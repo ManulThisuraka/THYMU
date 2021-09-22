3eab41dc8c02527c71d45799fbf715e173f2b7f9
Manul Thisuraka <55666581+Mavian69@users.noreply.github.com>
2021-09-22 21:50:23 +0530

    Hello THYMU!

---
+++ app/src/main/java/com/example/thymu/MainMain.java
@@ -0,0 +1,60 @@
+package com.example.thymu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+public class MainMain extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    personAdapter adapter; // Create Object of the Adapter class
+    DatabaseReference databaseReference; // Create object of the
+    // Firebase Realtime Database
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_main);
+
+        // Create a instance of the database and get
+        // its reference
+        databaseReference = FirebaseDatabase.getInstance("https://thymu-9c71c-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("user");
+
+        recyclerView = findViewById(R.id.recycler1);
+
+        // To display the Recycler view linearly
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+        FirebaseRecyclerOptions<person> options = new FirebaseRecyclerOptions.Builder<person>().setQuery(databaseReference, person.class).build();
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        adapter = new personAdapter(options);
+        // Connecting Adapter class with the Recycler view*/
+        recyclerView.setAdapter(adapter);
+    }
+
+    // Function to tell the app to start getting
+    // data from database on starting of the activity
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    // Function to tell the app to stop getting
+    // data from database on stopping of the activity
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
---
+++ app/src/main/java/com/example/thymu/person.java
@@ -0,0 +1,47 @@
+package com.example.thymu;
+
+
+public class person
+{
+    // Variable to store data corresponding
+    // to firstname keyword in database
+    private String firstname;
+
+    // Variable to store data corresponding
+    // to lastname keyword in database
+    private String lastname;
+
+    // Variable to store data corresponding
+    // to age keyword in database
+    private int age;
+
+    // Mandatory empty constructor
+    // for use of FirebaseUI
+    public person() {}
+
+    // Getter and setter method
+    public String getFirstname()
+    {
+        return firstname;
+    }
+    public void setFirstname(String firstname)
+    {
+        this.firstname = firstname;
+    }
+    public String getLastname()
+    {
+        return lastname;
+    }
+    public void setLastname(String lastname)
+    {
+        this.lastname = lastname;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+}
